aa7b34aa7c31f8de45d958addf4f1a225f34f079
MarieDaPiedade <68291195+MarieDaPiedade@users.noreply.github.com>
2022-02-28 16:59:39 +0100

    Ajout aos animation

--- src/components/Apropos.jsx
+++ src/components/Apropos.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../style/Apropos.css';
+import AOS from 'aos';
+
+AOS.init();
 
 export default function Apropos() {
   return (
     <div className="about">
-        <h1>À PROPOS</h1>
-        <p className="desc-apropos">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas ultrices risus rutrum sed
+        <h1 data-aos="flip-up" data-aos-duration="2000">À PROPOS</h1>
+        <p className="desc-apropos" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas ultrices risus rutrum sed
 quis donec eu. Dolor proin at est sed. Proin amet, gravida integer netus vulputate ut. 
 Bibendum consequat, est ac nisl dictum suscipit imperdiet sed. 
 Habitant lacinia sed adipiscing sit fermentum at amet. At vulputate nunc pharetra vulputate.
---
+++ src/components/Projets.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AOS from 'aos';
+
+AOS.init();
+
+export default function Projets() {
+  return (
+    <div className="projects">
+        <h1 data-aos="flip-up" data-aos-duration="2000">PROJETS</h1>
+    </div>
+  )
+}
