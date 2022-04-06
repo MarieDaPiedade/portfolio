eafc29e8824ced7ec630bc621ed43e467abb0d20
MarieDaPiedade <marie@dapiedade.org>
2022-04-06 22:30:12 +0200

    update projects en json

--- src/components/Projets.jsx
+++ src/components/Projets.jsx
@@ -1,36 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import AOS from 'aos';
 import '../style/Projets.css'
 
 AOS.init();
 
 export default function Projets() {
+
+    const [data, setData] = useState([]);
+
+    const getData = () => {
+        fetch('projects.json', 
+        {headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'}})
+        .then(function(response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function(myJson) {
+            console.log(myJson);
+            setData(myJson);
+        })
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+
+
   return (
     <div className="projects">
         <h1 data-aos="flip-up" data-aos-duration="2000">PROJETS</h1>
-        <div className="project">
-            <img src="assets/blog.png" alt="photo du projet" className="photo-project"/>
-            <div className="project-content">
-                <h2>NOM DU PROJET</h2>
-                <p className="text-project">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                A, auctor aliquam vestibulum, est pellentesque. Aliquam 
-                tellus congue montes, sed lacus, arcu.
-                </p>
+        {
+            data && data.length>0 && data.map((item => 
+        <div className={item.id % 2 === 0 ? "project-bis" : "project"}>
+            <img src={item.picture} alt="photo du projet" className="photo-project"/>
+            <div className={item.id % 2 === 0 ? "project-content-bis" : "project-content"}>
+                <h2>{item.title}</h2>
+                <p className="text-project">{item.content}</p>
                 <div className="button-techno">
-                    <p className="techno">Techno</p>
-                    <p className="techno">Techno</p>
-                    <p className="techno">Techno</p>
-                    <p className="techno">Techno</p>
-                    <p className="techno">Techno</p>
+                    {item.technos.map((value, index) => {
+                        return <p className="techno" key={index}>{value}</p>
+                    })}
                 </div>
                 <div className="icone-div">
                     <img className="icone" src="assets/github.svg" alt="GitHub" />
                     <img className="icone" src="assets/link.svg" alt="LinkedIn" />
                 </div>
             </div>
-
         </div>
+                ))
+        }
     </div>
   )
 }
